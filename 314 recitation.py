fc54aeb2242bac8c3858db37b5a3ce388c965eb6
Xi <63832451+siayann@users.noreply.github.com>
2021-09-08 20:12:51 -0400

    Add files via upload

---
+++ 314 recitation.py
@@ -0,0 +1,10 @@
+points_obtained=12
+total_point=50
+percentage=(points_obtained/total_point)*100
+
+print("the percentage is ", percentage,"%")
+
+x='I am a UB student'
+print(x[0])
+print(x[3])
+print(x[-1])
